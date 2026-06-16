81762eef313bd6a2da32f5852729be231a8d391d
Rugved Kadam <147609066+rugvedkadam7@users.noreply.github.com>
2026-06-16 23:04:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const form = document.getElementById("transaction-form");
+const description = document.getElementById("description");
+const amount = document.getElementById("amount");
+const transactionList = document.getElementById("transaction-list");
 
+form.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    console.log("Form Submitted");
+
+});
